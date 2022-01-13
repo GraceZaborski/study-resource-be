086fb762bb8824f7e40b4d1ae380482712dd0ace
Matt Phan <matthewmnphan@outlook.com>
2022-01-13 17:45:29 +0000

    all endpoints except /study_list

--- src/server.ts
+++ src/server.ts
@@ -29,15 +29,23 @@ export const client = new Client(dbConfig);
 client.connect();
 
 // interfaces
-export interface Comments {
-  resource_id: number;
+export interface Comment {
   author_id: number;
   comment_text: string;
 }
 
+export interface Like {
+  author_id: number;
+  liked: boolean;
+}
+
+export interface Tag {
+  tag_name: string;
+  tag_colour: string;
+}
+
 export interface StudyList {
   resource_id: number;
-  user_id: number;
   studied: boolean;
 }
 
@@ -45,9 +53,12 @@ export interface Resource {
   author_id: number;
   title: string;
   description: string;
+  type: string;
   recommended: string;
   url: string;
 }
+
+// <----------------------------------- /resources -------------------------------------------->
 
 // get all resources
 app.get("/resources", async (req, res) => {
@@ -101,40 +112,7 @@ app.get<{ id: number }>("/resources/:id/tags", async (req, res) => {
   });
 });
 
-// get all users
-app.get("/users", async (req, res) => {
-  const dbres = await client.query("SELECT * FROM users");
-  res.status(200).json({
-    status: "success",
-    message: "Retrieved all users",
-    data: dbres.rows,
-  });
-});
-
-// get all tags
-app.get("/tags", async (req, res) => {
-  //do we need just tag name?
-  const dbres = await client.query("SELECT * FROM tags");
-  res.status(200).json({
-    status: "success",
-    message: "Retrieved all tags",
-    data: dbres.rows,
-  });
-});
-// get a tag with tag_id
-app.get<{ id: number }>("/tags/:id", async (req, res) => {
-  const { id } = req.params;
-  const dbres = await client.query("SELECT * FROM tags WHERE tag_id = $1", [
-    id,
-  ]);
-  res.status(200).json({
-    status: "success",
-    message: "Retrieved one tag",
-    data: dbres.rows,
-  });
-});
-
-// get all comments to an associated resource
+// get all comments associated with a specific resource
 app.get<{ id: number }>("/resources/:id/comments", async (req, res) => {
   const { id } = req.params;
   const dbres = await client.query(
@@ -151,6 +129,231 @@ app.get<{ id: number }>("/resources/:id/comments", async (req, res) => {
     data: dbres.rows,
   });
 });
+
+//add a new comment associated with a resource
+app.post<{ id: number }, {}, Comment>(
+  "/resources/:id/comments",
+  async (req, res) => {
+    const { id } = req.params;
+    const { author_id, comment_text } = req.body;
+    const dbres = await client.query(
+      "INSERT INTO comments (resource_id, author_id, comment_text) VALUES ($1, $2, $3) RETURNING *",
+      [id, author_id, comment_text]
+    );
+    res.status(201).json({
+      status: "success",
+      message: `Added a new comment to resource with id ${id}`,
+      data: dbres.rows[0],
+    });
+  }
+);
+
+// add an entry into likes table to like or dislike
+app.post<{ id: number }, {}, Like>("/resources/:id/likes", async (req, res) => {
+  const { id } = req.params;
+  const { author_id, liked } = req.body;
+  await client.query(
+    "INSERT INTO likes (author_id, resource_id, liked) VALUES ($1, $2, $3)",
+    [author_id, id, liked]
+  );
+  res.status(200).json({
+    status: "success",
+    message: `${liked ? "Liked" : "Disliked"} resource with id ${id}`,
+    data: {
+      resource_id: id,
+      author_id,
+    },
+  });
+});
+
+//associates existing tags to a resource
+app.post<{ id: number }, {}, { tag_ids: number[] }>(
+  "/resources/:id/tags",
+  async (req, res) => {
+    const { id } = req.params;
+    const { tag_ids } = req.body;
+    console.log("type of tag_ids", typeof tag_ids);
+    for (const tag_id of tag_ids) {
+      await client.query(
+        "INSERT INTO resource_tags (tag_id, resource_id) VALUES ($1, $2)",
+        [tag_id, id]
+      );
+    }
+    const dbres = await client.query(
+      "SELECT rt.*, t.tag_name, t.tag_colour FROM resource_tags rt JOIN tags t ON rt.tag_id = t.tag_id"
+    );
+    res.status(201).json({
+      status: "success",
+      message: "Associated existing tags with an existing resource",
+      data: dbres.rows,
+    });
+  }
+);
+
+//add a new resource
+app.post<{}, {}, Resource>("/resources", async (req, res) => {
+  const { author_id, title, description, type, recommended, url } = req.body;
+  const dbres = await client.query(
+    "INSERT INTO resources (author_id, title, description, type, recommended, url) VALUES\
+    ($1, $2, $3,$4, $5, $6) RETURNING *",
+    [author_id, title, description, type, recommended, url]
+  );
+  res.status(201).json({
+    status: "success",
+    message: "Added a new resource",
+    data: dbres.rows,
+  });
+});
+
+//update the to_study status of a specific resource in a specific user's study list *PARKED*
+// app.put<{}, {}, Resource>("/resources/update", async (req, res) => {
+//   const { title, description, recommended, url } = req.body;
+//   const dbres = await client.query(
+//     "UPDATE resources SET title=$1, description=$2, recommended=$3, url=$4 WHERE id=$5 RETURNING *",
+//     [title, description, recommended, url]
+//   );
+//   res.status(200).json({
+//     status: "success",
+//     message:
+//       "Updated the to_study status of a specific resource in a specific user's study list",
+//     data: dbres.rows,
+//   });
+// });
+
+// // deletes a resource to the study list of a specific user *PARKED*
+// app.delete<{ id: number }>("/resources/:id", async (req, res) => {
+//   //endpoint naming conventions
+//   const { id } = req.params;
+//   const resourceExists = await client.query(
+//     "SELECT * FROM resources WHERE id= $1",
+//     [id]
+//   );
+//   if (resourceExists.rowCount !== 0) {
+//     const dbres = await client.query(
+//       "DELETE FROM resources WHERE id = $1 RETURNING *",
+//       [id]
+//     );
+//     res.status(200).json({
+//       status: "success",
+//       message: "Deleted a resource",
+//       data: dbres.rows,
+//     });
+//   } else {
+//     res.status(409).json({
+//       status: "fail",
+//       message: "There was no resource to delete",
+//     });
+//   }
+// });
+
+// <----------------------------------- /users -------------------------------------------->
+
+// get all users
+app.get("/users", async (req, res) => {
+  const dbres = await client.query("SELECT * FROM users");
+  res.status(200).json({
+    status: "success",
+    message: "Retrieved all users",
+    data: dbres.rows,
+  });
+});
+
+// <----------------------------------- /tags -------------------------------------------->
+
+// get all tags
+app.get("/tags", async (req, res) => {
+  //do we need just tag name?
+  const dbres = await client.query("SELECT * FROM tags");
+  res.status(200).json({
+    status: "success",
+    message: "Retrieved all tags",
+    data: dbres.rows,
+  });
+});
+
+// get a tag with tag_id
+app.get<{ id: number }>("/tags/:id", async (req, res) => {
+  const { id } = req.params;
+  const dbres = await client.query("SELECT * FROM tags WHERE tag_id = $1", [
+    id,
+  ]);
+  res.status(200).json({
+    status: "success",
+    message: "Retrieved one tag",
+    data: dbres.rows,
+  });
+});
+
+//add a new tag when it doesn't already exist --> need to make an array of tags
+app.post<{}, {}, { tags: Tag[] }>("/tags", async (req, res) => {
+  const { tags } = req.body;
+  const successResponse = [];
+  const failResponse = [];
+  for (const tag of tags) {
+    const tagExists = await client.query(
+      "SELECT * FROM tags WHERE tag_name = $1",
+      [tag.tag_name]
+    );
+    if (tagExists.rowCount === 0) {
+      const dbres = await client.query(
+        "INSERT INTO tags (tag_name, tag_colour) VALUES ($1, $2) RETURNING *",
+        [tag.tag_name, tag.tag_colour]
+      );
+      successResponse.push(dbres.rows[0]);
+    } else {
+      failResponse.push(tagExists.rows[0]);
+    }
+  }
+  if (failResponse.length === 0) {
+    res.status(200).json({
+      status: "success",
+      message: "all tags were successfully added",
+      data: successResponse,
+    });
+  } else if (successResponse.length > 0) {
+    res.status(409).json({
+      status: "partial success",
+      message: "some tags were successfully added",
+      data: {
+        added: successResponse,
+        duplicates: failResponse,
+      },
+    });
+  } else {
+    res.status(400).json({
+      status: "complete failure",
+      message: "no tags were successfully added",
+      data: {
+        duplicates: failResponse,
+      },
+    });
+  }
+});
+
+//   const { tag_name, tag_colour } = req.body;
+//   const tagExists = await client.query(
+//     "SELECT * FROM tags WHERE tag_name = $1",
+//     [tag_name]
+//   );
+//   if (tagExists.rowCount === 0) {
+//     const dbres = await client.query(
+//       "INSERT INTO tags (tag_name, tag_colour) VALUES ($1, $2) RETURNING *",
+//       [tag_name, tag_colour]
+//     );
+//     res.status(201).json({
+//       status: "success",
+//       message: "Added a new tag",
+//       data: dbres.rows,
+//     });
+//   } else {
+//     res.status(409).json({
+//       //or 500?
+//       status: "fail",
+//       message: "Tag already exists",
+//       data: tagExists.rows,
+//     });
+//   }
+// });
 
 // get the study list resources for an associated user
 app.get<{ user_id: number }>("/study_list/:user_id", async (req, res) => {
@@ -169,22 +372,47 @@ app.get<{ user_id: number }>("/study_list/:user_id", async (req, res) => {
   });
 });
 
+//add a resource to the study list of a specific user
+app.post<{ user_id: number }, {}, StudyList>(
+  "/study_list/:user_id",
+  async (req, res) => {
+    //need to alter the front-end so that you can't add a specific resource to a study list more than once
+    const { user_id } = req.params;
+    const { resource_id, studied } = req.body;
+    const dbres = await client.query(
+      "INSERT INTO study_list (user_id, resource_id, studied) VALUES ($1, $2, $3) RETURNING *",
+      [user_id, resource_id, studied]
+    );
+    res.status(201).json({
+      status: "success",
+      message: "Added a resource to the study list of a specific user",
+      data: dbres.rows,
+    });
+  }
+);
+
+// <---
+
 // update the studied status of a specific resource in a specific user's study list
-app.put<{}, {}, StudyList>("/study_list/update", async (req, res) => {
-  //when do we use req.body and when to use req.params
-  const { user_id, resource_id, studied } = req.body;
-  const dbres = await client.query(
-    "UPDATE study_list SET studied = $1 WHERE user_id = $2 and resource_id = $3 RETURNING *",
-    [studied, user_id, resource_id]
-    //how come the to_study default is set to false in the study_list table?
-  );
-  res.status(200).json({
-    status: "success",
-    message:
-      "Updated the to_study status of a specific resource in a specific user's study list",
-    data: dbres.rows,
-  });
-});
+app.put<{ user_id: number }, {}, StudyList>(
+  "/study_list/:user_id",
+  async (req, res) => {
+    //when do we use req.body and when to use req.params
+    const { user_id } = req.params;
+    const { resource_id, studied } = req.body;
+    const dbres = await client.query(
+      "UPDATE study_list SET studied = $1 WHERE user_id = $2 and resource_id = $3 RETURNING *",
+      [studied, user_id, resource_id]
+      //how come the to_study default is set to false in the study_list table?
+    );
+    res.status(200).json({
+      status: "success",
+      message:
+        "Updated the to_study status of a specific resource in a specific user's study list",
+      data: dbres.rows,
+    });
+  }
+);
 
 //update the to_study status of a specific resource in a specific user's study list [IN DEVELOPMENT]
 // app.put<{}, {}, Resource>("/resources/update", async (req, res) => {
@@ -200,126 +428,6 @@ app.put<{}, {}, StudyList>("/study_list/update", async (req, res) => {
 //     data: dbres.rows,
 //   });
 // });
-
-//add a new tag when it doesn't already exist
-app.post("/tags", async (req, res) => {
-  const { tag } = req.body;
-  const tagExists = await client.query(
-    "SELECT * FROM tags WHERE tag_name = $1",
-    [tag]
-  );
-  if (tagExists.rowCount === 0) {
-    const dbres = await client.query(
-      "INSERT INTO tags (tag_name) VALUES ($1) RETURNING *",
-      [tag]
-    );
-    res.status(201).json({
-      status: "success",
-      message: "Added a new tag",
-      data: dbres.rows,
-    });
-  } else {
-    res.status(409).json({
-      //or 500?
-      status: "fail",
-      message: "Tag already exists",
-      data: tagExists.rows,
-    });
-  }
-});
-
-//add a new comment associated with a resource
-app.post<{}, {}, Comments>("/comments", async (req, res) => {
-  const { resource_id, author_id, comment_text } = req.body;
-  const dbres = await client.query(
-    "INSERT INTO comments (resource_id, author_id, comment_text) VALUES ($1, $2, $3) RETURNING *",
-    [resource_id, author_id, comment_text]
-  );
-  res.status(201).json({
-    status: "success",
-    message: "Added a new comment",
-    data: dbres.rows,
-  });
-});
-
-//add a resource to the study list of a specific user
-app.post<{}, {}, StudyList>("/study_list", async (req, res) => {
-  //when do we use req.body and when to use req.params?
-  //need to alter the front-end so that you can't add a specific resource to a study list more than once
-  const { user_id, resource_id, studied } = req.body;
-  const dbres = await client.query(
-    "INSERT INTO study_list (user_id, resource_id, to_study) VALUES ($1, $2, $3) RETURNING *",
-    [user_id, resource_id, studied]
-    //how come the to_study default is set to false in the study_list table?
-  );
-  res.status(201).json({
-    status: "success",
-    message: "Added a resource to the study list of a specific user",
-    data: dbres.rows,
-  });
-});
-
-// add an entry into likes table to like or dislike
-app.post<{ id: number }, {}, { user_id: number; liked: boolean }>(
-  "/resources/:id/likes",
-  async (req, res) => {
-    const { id } = req.params;
-    const { user_id, liked } = req.body;
-    const dbres = await client.query(
-      "INSERT INTO likes (author_id, resource_id, liked) VALUES ($1, $2, $3)",
-      [user_id, id, liked]
-    );
-    res.status(200).json({
-      status: "success",
-      message: "Updated the number of likes for a spific resource",
-      data: {
-        resource_id: id,
-        user_id,
-      },
-    });
-  }
-);
-
-//add a new resource
-app.post<{}, {}, Resource>("/add_resource", async (req, res) => {
-  //should date_added be default? how does date work in SQL? Can we use CURRENTDATE as default type?
-  //change liked to default 0
-  const { author_id, title, description, recommended, url } = req.body;
-  const dbres = await client.query(
-    "INSERT INTO resources (author_id, title, description, recommended, url, likes) VALUES\
-    ($1, $2, $3,$4, $5, $6) RETURNING *",
-    [author_id, title, description, recommended, url]
-  );
-  res.status(201).json({
-    status: "success",
-    message: "Added a new resource",
-    data: dbres.rows,
-  });
-});
-
-//associates an existing tag to a resource
-app.post<{ id: number }, {}, { tag_ids: number[] }>(
-  "/resources/:id/tags",
-  async (req, res) => {
-    const { id } = req.params;
-    const { tag_ids } = req.body;
-    console.log("type of tag_ids", typeof tag_ids);
-    for (const tag_id of tag_ids) {
-      await client.query(
-        "INSERT INTO resource_tags (tag_id, resource_id) VALUES ($1, $2)",
-        [tag_id, id]
-      );
-    }
-    const dbres = await client.query(
-      "SELECT rt.*, t.tag_name, t.tag_colour FROM resource_tags rt JOIN tags t ON rt.tag_id = t.tag_id"
-    );
-    res.status(201).json({
-      status: "success",
-      message: "Added a new resource",
-      data: dbres.rows,
-    });
-  }
-);
 
 // delete an entry in like table to unlike and undislike
 app.delete<{ id: number }, {}, { user_id: number }>(
@@ -340,58 +448,31 @@ app.delete<{ id: number }, {}, { user_id: number }>(
 );
 
 //add a resource to the study list of a specific user
-app.delete<{}, {}, StudyList>("/study_list/delete", async (req, res) => {
-  //endpoint naming conventions
-  const { user_id, resource_id } = req.body;
-  const resourceExists = await client.query(
-    "SELECT * FROM study_list WHERE user_id = $1 and resource_id = $2",
-    [user_id, resource_id]
-  );
-  if (resourceExists.rowCount !== 0) {
-    const dbres = await client.query(
-      "DELETE FROM study_list WHERE user_id = $1 and resource_id = $2 RETURNING *",
-      [user_id, resource_id]
-    );
-    res.status(200).json({
-      status: "success",
-      message: "Deleted a resource from study list",
-      data: dbres.rows,
-    });
-  } else {
-    res.status(409).json({
-      //or 500?
-      status: "fail",
-      message: "There was no resource to delete",
-    });
-  }
-});
-
-//add a resource to the study list of a specific user
-app.delete<{ id: number }>("/resources/delete/:id", async (req, res) => {
-  //endpoint naming conventions
-  const { id } = req.params;
-  const resourceExists = await client.query(
-    "SELECT * FROM resources WHERE id= $1",
-    [id]
-  );
-  if (resourceExists.rowCount !== 0) {
-    const dbres = await client.query(
-      "DELETE FROM resources WHERE id = $1 RETURNING *",
-      [id]
-    );
-    res.status(200).json({
-      status: "success",
-      message: "Deleted a resource",
-      data: dbres.rows,
-    });
-  } else {
-    res.status(409).json({
-      //or 500?
-      status: "fail",
-      message: "There was no resource to delete",
-    });
-  }
-});
+// app.delete<{}, {}, StudyList>("/study_list/delete", async (req, res) => {
+//   //endpoint naming conventions
+//   const { user_id, resource_id } = req.body;
+//   const resourceExists = await client.query(
+//     "SELECT * FROM study_list WHERE user_id = $1 and resource_id = $2",
+//     [user_id, resource_id]
+//   );
+//   if (resourceExists.rowCount !== 0) {
+//     const dbres = await client.query(
+//       "DELETE FROM study_list WHERE user_id = $1 and resource_id = $2 RETURNING *",
+//       [user_id, resource_id]
+//     );
+//     res.status(200).json({
+//       status: "success",
+//       message: "Deleted a resource from study list",
+//       data: dbres.rows,
+//     });
+//   } else {
+//     res.status(409).json({
+//       //or 500?
+//       status: "fail",
+//       message: "There was no resource to delete",
+//     });
+//   }
+// });
 
 //Start the server on the given port
 const port = process.env.PORT;
